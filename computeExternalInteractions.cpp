0599caf3ecb5d6c8fa5aa1ff80f7ae86e3d8211d
jalogan <69821981+jalogan@users.noreply.github.com>
2026-03-02 11:56:56 -0700

    Add files via upload

--- computeExternalInteractions.cpp
+++ computeExternalInteractions.cpp
@@ -1,6 +1,6 @@
 #include "InteractionManager.h"
 #include "Vector3D.h"
-
+#include <iostream>
 
 // Include any external forces, e.g., central force, on each sphere
 
@@ -13,15 +13,27 @@ void InteractionManager::computeExternalInteractions()
 	if(central_force_mag > 0.0)
 	{
 		double scale;
-		Vector3D unitba;
+		//Vector3D unitba;
 		Vector3D centralF;
 
 		for(auto& sph_ptr : sim->spheres)
 		{	
 			auto* sph = sph_ptr.get();
 
+
+
+			Vector3D r = -sph->position;
+			double mag = r.norm();
+
+			Vector3D unitba(0,0,0);
+
+			if (mag > 1e-12)
+				unitba = r * (1.0 / mag);
+/*
 			// unit vector from particle to origin (assumed to be approximate COM)
+			//sph->position.print();
 			unitba = -sph->position.normalized();
+*/
 
 			scale = central_force_mag * pow(sph->diameter / sim->max_diameter, 2.25);
 			centralF = unitba*scale;
